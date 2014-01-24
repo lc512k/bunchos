faddcd2a4a1819d3607acab6e81d5a82b7a47a3e
Laura Carvajal <laura.carvajal@gmail.com>
2014-01-24 23:15:25 +0100

    playing around with mockup

--- app/main.js
+++ app/main.js
@@ -38,50 +38,63 @@ define(['jquery', 'mustache', 'foundation', 'modernizr', 'reveal', 'text!templat
 
         $form.data('bunchos-trigger', currentTargetId);
 
-        bindFormEvents();
-      });
+        console.log('trigger?', $form.data('bunchos-trigger'));
+        var $formTitle = $('#bunchos-form-title');
 
-
-      $cancelFormBtn.on('click', function() {
-        $form.hide();
-      });
-
-      $saveFormBtn.on('click', function() {
-        $form.hide();
-        var trigger = $form.data('bunchosTrigger');
-        //console.log("$donations", uniformTemplate, Mustache.render(uniformTemplate))
-        //$donations.append(Mustache.render(uniformTemplate));
-        if (trigger === "add-donation") {
-          $(Mustache.render(uniformTemplate)).appendTo($donations).fadeIn(300);
-        } else if (trigger === "add-request") {
-
-          $(Mustache.render(uniformTemplate)).appendTo($requests).fadeIn(300);
+        if ($form.data('bunchos-trigger').indexOf('donation') > 0) {
+          $formTitle.html('New Donation');
+        } else {
+          $formTitle.html('New Request');
         }
-        if (typeof(Storage) !== "undefined") {
-          localStorage.uniform = "hello"
-        }
-      })
-    };
-
-    bindFormEvents = function() {
-      console.log('trigger?', $form.data('bunchos-trigger'));
-      var $formTitle = $('#bunchos-form-title');
-
-      if ($form.data('bunchos-trigger').indexOf('donation')) {
-        $formTitle.html('New Donation');
-      } else {
-        $formTitle.html('New Request');
-      }
+      });
 
       $formInput = $('#bunchos-form-input');
       $formOutput = $('#bunchos-form-output');
       $anyFormNode = $('[data-bunchos-node]');
       $anyFormElement = $('[data-bunchos-element]');
 
-      $anyFormElement.on('click', function() {
-        console.log('this', this);
+      $anyFormElement.on('click', function(e) {
+        var currentDiv = $(this).parent().attr('id');
+        var nextDivId = +currentDiv.substring(currentDiv.length - 1, currentDiv.length) + 1;
         $formOutput.append($.clone(this));
+        console.log($('#bunchos-element-' + nextDivId), nextDivId);
+        $('#bunchos-element-' + nextDivId).show();
+        //$(this).parent().hide();
       });
+
+      $anyFormNode.on('click', function() {
+        var currentDiv = $(this).parent().attr('id');
+        var nextDivId = +currentDiv.substring(currentDiv.length - 1, currentDiv.length) + 1;
+        console.log($('#bunchos-element-' + nextDivId), nextDivId);
+        $('#bunchos-element-' + nextDivId).show();
+        //$(this).parent().hide();
+
+      });
+
+      $cancelFormBtn.on('click', function() {
+        $anyFormElement.hide();
+        $form.hide();
+        $formOutput.html('');
+      });
+
+      $saveFormBtn.on('click', function() {
+        $anyFormElement.hide();
+        $form.hide();
+        //var trigger = $form.data('bunchosTrigger');
+        //console.log("$donations", uniformTemplate, Mustache.render(uniformTemplate))
+        //$donations.append(Mustache.render(uniformTemplate));
+        //if (trigger === "add-donation") {
+        //  $.clone($('#bunchos-form-output')).appendTo($donations).fadeIn(300);
+        //} else /*if (trigger === "add-request")*/ {
+        var clone = $($('#bunchos-form-output').html());
+        console.log('clone', clone)
+        clone.appendTo($requests).fadeIn(300);
+        //}
+        // if (typeof(Storage) !== "undefined") {
+        //   localStorage.uniform = "hello"
+        // }
+        $formOutput.html('');
+      })
     };
 
     init = function() {
