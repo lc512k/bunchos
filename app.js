19fd96ce8d08ff3e9e14127a0b8a010edeb0654a
Mami <mami@admins-MacBook-Air.local>
2015-11-26 15:37:20 +0000

    angular project

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
-/* global requirejs */
 
-requirejs.config({
-  baseUrl: 'lib',
-  paths: {
-    app: '../app',
-    templates: '../templates',
-    data: '../data',
-    extras: '../extras',
-    jquery: 'jquery-2.1.0',
-    mustache: 'mustache',
-    foundation: 'foundation/foundation',
-    text: 'text',
-    tv4: 'tv4',
-    modernizr: 'modernizr',
-    reveal: 'foundation/foundation.reveal'
-  },
-  shim: {
-    'foundation': {
-      deps: ['jquery', 'modernizr']
-    },
-    'modernizr': {
-      deps: ['jquery']
-    },
-    'reveal': {
-      deps: ['jquery', 'foundation'],
-      exports: 'reveal'
-    }
-  }
-});
 
-requirejs(['app/main', 'extras/google']);
+angular.module('root', [])
+    .controller('index', ['$scope', function ($scope) {
+        $scope.message = 'Hello';
+    }]);
+
+
+
